662c9dcbbe1414a5d569a763997c74a027c0e88f
Andrew Davisson <akdavisson4@gmail.com>
2019-09-05 09:40:08 -0600

    Added sessions controller and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get '/' => 'welcome#home'
+  root to: 'welcome#home'
   resources :check_out_logs
   resources :authors
   resources :genres
   resources :books
   resources :users
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
