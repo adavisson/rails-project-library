f423775384cc32a64265bb1f298b7c4b8c4a347f
Andrew Davisson <akdavisson4@gmail.com>
2019-09-05 10:14:25 -0600

    Added basic book contoller actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,21 @@
 class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+    book = Book.create(book_params)
+    redirect_to book_path(book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def
+    params.require(:book).permit(:title, :author_id, :genre_id, :checked_out, :present_user_id)
+  end
+
 end
