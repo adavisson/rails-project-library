e392fa0a3b9a9c713669854448269c12c2dd5d61
Andrew Davisson <akdavisson4@gmail.com>
2019-09-05 11:59:40 -0600

    Added checkout to views. needs to be altered

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/signout' => 'sessions#destroy'
-  get '/books/:id/check_out_page' => 'books#check_out_page'
-  post '/books/:id/check_out' => 'books#check_out'
+  get '/books/:id/check_out_page' => 'books#check_out_page', as: 'check_out_page'
+  post '/books/:id/check_out' => 'books#check_out', as: 'check_out'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
