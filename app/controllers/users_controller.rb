bb3fb21a04fbf7767e6447362aa86c7b87d120f7
Andrew Davisson <akdavisson4@gmail.com>
2019-09-05 09:48:20 -0600

    Added users_controller actions for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  before_action :require_login
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = User.craete(user_params)
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :email, :password, :password_confirmation, :librarian)
+  end
+
 end
