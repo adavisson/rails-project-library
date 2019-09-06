e5fddfbcd04a741e0eea55c96be0c79a7a53ffd4
Andrew Davisson <akdavisson4@gmail.com>
2019-09-06 11:13:53 -0600

    Progress on oauth and removed age from user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,36 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    return head(:forbidden) unless user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to user_path(user)
+    if auth
+      # Logged in via oauth
+      raise auth.inspect
+      #name: Andrew Davisson
+      #email: akdavisson4@gmail.com
+      user = User.find_or_create_by(uid: auth[:uid]) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+    else
+      # Normal login flow
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        render 'sessions/new'
+      end
+    end
   end
 
   def destroy
     session.delete :user_id
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   get '/signin' => 'sessions#new'
-  post '/signin' => 'sessions#create'
+  post '/sessions' => 'sessions#create'
   post '/signout' => 'sessions#destroy'
+  get '/auth/google_oauth2/callback' => 'sessions#create'
+  
+  # Rewrite to be part of check_out_log_controller
   get '/books/:id/check_out_page' => 'books#check_out_page', as: 'check_out_page'
   post '/books/:id/check_out' => 'books#check_out', as: 'check_out'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
