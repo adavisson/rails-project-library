380575716e1fcac4a0a0bf5c2a6d33df98e6a04d
Andrew Davisson <akdavisson4@gmail.com>
2019-09-07 18:22:25 -0600

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }
   
   has_many :check_out_logs
   has_many :books, through: :check_out_logs
