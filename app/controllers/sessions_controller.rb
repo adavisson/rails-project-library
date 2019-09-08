e91ce23859117f77cae5f3f4de6b7f29cd493ec3
Andrew Davisson <akdavisson4@gmail.com>
2019-09-07 20:13:27 -0600

    Edited login to notify user of wrong credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
@@ -11,12 +12,13 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
       # Normal login flow
-      user = User.find_by(email: params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
         redirect_to root_path
       else
-        render 'sessions/new'
+        flash[:notice] = "Incorrect username or password"
+        render :new
       end
     end
   end
