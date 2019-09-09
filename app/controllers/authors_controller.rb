ddc32ad0e74242087fb18e0b8f99990afb3af2f0
Andrew Davisson <akdavisson4@gmail.com>
2019-09-09 17:16:58 -0600

    fixed variable error

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -14,7 +14,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
     if @author.save
-      redirect_to author_path(author)
+      redirect_to author_path(@author)
     else
       render :new
     end
